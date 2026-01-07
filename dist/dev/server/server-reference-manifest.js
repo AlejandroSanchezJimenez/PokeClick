75c3129dc70aa0ab8e2eaea422dc728ee53d7d68
alejandroUnita <alejandro@unita.es>
2026-01-07 15:41:34 +0100

    fix

--- dist/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"CsnQrIKtMjgKFvS9DPWuv7xYyxtb5mSArQhloFsq1/0=\"\n}"
--- dist/dev/static/chunks/app_page_a0160717.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_407bb824._.js",
-  "static/chunks/node_modules_5b397f6a._.js",
-  "static/chunks/node_modules_react-toastify_dist_ReactToastify_487faac8.css"
-],
-    source: "dynamic"
-});
