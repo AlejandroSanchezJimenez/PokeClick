7f098c5b41db13d11cdaf304c1b10a191a295131
alejandroUnita <alejandro@unita.es>
2026-01-07 14:41:24 +0100

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  /* config options here */
-  reactCompiler: true,
-};
 
-export default nextConfig;
+const isProd = process.env.NODE_ENV === 'production'
+
+const nextConfig = {
+  basePath: isProd ? '/AlejandroSanchezJimenez' : '',
+  output: 'export',
+  distDir: 'dist',
+  images: {
+    unoptimized: true,
+  },
+}
+
+module.exports = nextConfig
