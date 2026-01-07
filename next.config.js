179b8cda7e61b41b8c59df88c1b23cfb2ddc9046
alejandroUnita <alejandro@unita.es>
2026-01-07 15:00:08 +0100

    fix

--- next.config.js
+++ next.config.js
@@ -1,14 +1,13 @@
 /** @type {import('next').NextConfig} */
-
 const isProd = process.env.NODE_ENV === 'production'
 
 const nextConfig = {
   basePath: isProd ? '/PokeClick' : '',
-  output: 'export',
-  distDir: 'dist',
+  output: 'export', // export estático
   images: {
     unoptimized: true,
   },
+  trailingSlash: true, // ayuda a GitHub Pages con rutas tipo /shop
 }
 
 module.exports = nextConfig
